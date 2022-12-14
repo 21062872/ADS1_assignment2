ffd2215551018a3df3d92be04a11f356a4e75d68
21062872 <117090971+21062872@users.noreply.github.com>
2022-12-14 14:42:28 +0000

    correlation heatmap

--- assignment2_answers.py
+++ assignment2_answers.py
@@ -4,11 +4,13 @@ Created on Sun Dec  4 12:09:55 2022
 
 @author: TharindaArachchi
 """
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import seaborn as sb
+import scipy.stats as stats
+
 
 def read_external_files(filename):
     '''
@@ -28,12 +30,10 @@ def read_external_files(filename):
     splitFileName = os.path.splitext(filename)
     fileExtension = splitFileName[1]
     
-    #World bank data is in csv, xml and excel formats only. Based on those formats reading 
-    #the files into dataframe
+    #World bank data is in csv, excel formats. Based on those formats reading the files into dataframe
     if (fileExtension == '.csv'):
         df_climt_chg = pd.read_csv(filename, skiprows=4)
         df_climt_chg_tp = df_climt_chg.set_index('Country Name').transpose()
-    ##TO DO :: XML read file
     elif (fileExtension == '.xls'):
         df_climt_chg = pd.read_excel(filename, skiprows=3)
         df_climt_chg_tp = df_climt_chg.set_index('Country Name').transpose()
@@ -47,7 +47,27 @@ def calculateAutoPct(values):
     def autopct(pct):
         return '{p:.2f}%'.format(p=pct)
     return autopct
-   
+
+def calculateStats(dataset):
+    '''
+    Read dataframe and calculate statistics
+
+    Parameters
+    ----------
+    dataframe
+
+    Returns
+    -------
+    calculated statistics
+
+    '''    
+    #Get numeric columns only
+    df_numeric = dataset.select_dtypes(include=np.number)
+    vmax = np.max(df_numeric, axis=1)
+    vmin = np.min(df_numeric, axis=1)
+    vmean = np.mean(df_numeric, axis=1)
+    return vmax, vmin, vmean
+
 #Executing the function to load external file to dataframe     
 df_climt_chg, df_climt_chg_tp = read_external_files('API_19_DS2_en_csv_v2_4700503.csv')    
 
@@ -57,7 +77,9 @@ countries = ['United Kingdom', 'India', 'Japan', 'China', 'Korea, Rep.',
 df_countries = df_climt_chg[df_climt_chg['Country Name'].isin(countries)]
 
 #Select records for specific indicators
-indecators = ['Urban population', 'Population, total', 'CO2 emissions (kt)', 'Forest area (% of land area)']
+indecators = ['CO2 emissions (kt)', 'Urban population', 'Population, total', 'Forest area (% of land area)',
+              'Arable land (% of land area)', 'Total greenhouse gas emissions (kt of CO2 equivalent)', 
+              'Methane emissions (kt of CO2 equivalent)', 'Mortality rate, under-5 (per 1,000 live births)']
 df_cntry_ind = df_countries[df_countries['Indicator Name'].isin(indecators)]
 #Select only columns after year 1990
 df_cntry_yrs = df_cntry_ind.loc[:,['Country Name', 'Indicator Name', '1990', '1991', '1992',	
@@ -80,7 +102,9 @@ df_fnl.replace("United States", "USA", inplace=True)
 df_fnl.replace("South Africa", "SA", inplace=True)
 df_fnl.replace("Korea, Rep.", "Korea", inplace=True)
 
-### START: Plot bar graph against urban population
+''' Plot: 1
+    Plot type: Bar chart
+    Plot name: Urban Population Over Time'''
 #Plot bar chart against indicator Urban population
 df_urbn_pop = df_fnl[df_fnl['Indicator Name'] == 'Urban population']
 
@@ -95,14 +119,12 @@ df_urbn_pop.plot(x='Country Name',
 # labeling the graph
 plt.xlabel('Country')
 plt.ylabel('Number of population')
-plt.legend(title ="Years")
-
-# save plot as .png
-plt.savefig('Urban Population Over Time.png')
+plt.legend(title ="Years", bbox_to_anchor =(0.8, 1, 0.3, 0))
 plt.show()
-### END: Plot bar graph against urban population
 
-### START: Plot bar graph against CO2 emission
+''' Plot: 2
+    Plot type: Bar chart
+    Plot name: CO2 emission Over Time'''
 #Plot bar chart against indicator CO2 emmision
 df_co2_emsn = df_fnl[df_fnl['Indicator Name'] == 'CO2 emissions (kt)']
 
@@ -118,13 +140,11 @@ df_co2_emsn.plot(x='Country Name',
 plt.xlabel('Country')
 plt.ylabel('CO2 emission')
 plt.legend(title ="Years", loc='upper right', bbox_to_anchor =(0.8, 1, 0.3, 0))
-
-# save plot as .png
-plt.savefig('CO2 emission Over Time.png')
 plt.show()
-### END: Plot bar graph against CO2 emission
 
-### START: Line chart to illustrate population growth over a time period
+''' Plot: 3
+    Plot type: Line chart
+    Plot name: Forest Area'''
 # create a new figure
 plt.figure(figsize=(8,5))
 # add a stylesheet
@@ -148,12 +168,11 @@ plt.xticks(df_ln_plot_tp.Year)
 plt.xlabel('Year')
 plt.ylabel('SQ(KM)')
 plt.legend(loc = 'lower right', title ="Countries")
-# save plot as .png
-plt.savefig('Forest Area.png', dpi=300)
 plt.show()
-### END: Line chart to illustrate population growth over a time period
 
-### START: Pie chart to illustrate portion of CO2 emission
+''' Plot: 4
+    Plot type: Pie chart
+    Plot name: CO2 emission in 2022'''
 df_co2_emsn_pc = df_fnl[df_fnl['Indicator Name'] == 'CO2 emissions (kt)']
 df_co2_emsn_pc = df_co2_emsn_pc.loc[:, ['Country Name', '2020']]
 
@@ -175,33 +194,17 @@ plt.legend(
           )   
 plt.tight_layout()
 plt.title("CO2 emission in 2022")
-plt.savefig('CO2 emission in 2022.png', dpi=300)
 plt.show()
 
-#Derive statistics
-co2_mean = df_co2_emsn_pc['2020'].mean()
-print('\nAverage of CO2 emission in 2022:', round(co2_mean,2))
-
-co2_max = df_co2_emsn_pc['2020'].max()
-print('\nMaximum portion of CO2 emission in 2022:', round(co2_max,2))
-
-co2_min = df_co2_emsn_pc['2020'].min()
-print('\nMinimum portion of CO2 emission in 2022:', round(co2_min,2))
-
-co2_sum = df_co2_emsn_pc['2020'].sum()
-print('\nTotal CO2 emission in 2022:', round(co2_sum,2))
-
-### END: Pie chart to illustrate portion of CO2 emission
-
-### START: Plot Co2 emission across years in total
+''' Plot: 4
+    Plot type: horizontal bar chart
+    Plot name: Average Co2 emission per year'''
 df_co2_emsn_tot = df_fnl[df_fnl['Indicator Name'] == 'CO2 emissions (kt)']
 df_co2_emsn_tot = df_co2_emsn_tot.loc[:,['Country Name', '1990', '1995', '2000', '2005', '2010', '2015', '2020']]
 #Sum up figures in each row
-df_co2_emsn_tot['Total'] = df_co2_emsn_tot.sum(axis=1)
-#Sum up figures in each row
-df_co2_emsn_tot['Total'] = df_co2_emsn_tot.sum(axis=1)
+df_co2_emsn_tot['Total'] = df_co2_emsn_tot.sum(axis=1, numeric_only=True)
 #Find average figures in each row
-df_co2_emsn_tot['Avg'] = df_co2_emsn_tot.mean(axis=1)
+df_co2_emsn_tot['Avg'] = df_co2_emsn_tot.mean(axis=1, numeric_only=True)
 #Sorting dataframe by average column
 df_co2_emsn_tot = df_co2_emsn_tot.sort_values(by='Avg')
 
@@ -210,40 +213,42 @@ plt.figure(figsize=(8,5))
 # add a stylesheet
 plt.style.use('ggplot')
 # plot the grapgh
-df_co2_emsn_tot.plot.barh('Country Name', 'Avg')
+df_co2_emsn_tot.plot.barh('Country Name', 'Avg', label='Avg CO2 emission',  color=(0.2, 0.4, 0.6, 0.6))
 # labeling the graph
 plt.ylabel('Country')
-plt.xlabel('Average Co2 emission(kt)')
+plt.xlabel('Co2 emission(kt)')
+plt.title('Average Co2 emission(kt) per year')
 plt.legend(loc = 'best')
-# save plot as .png
-plt.savefig('Average Co2 emission of period 1990-2020.png', dpi=300, bbox_inches='tight')
 plt.show()
-### END: Plot Co2 emission across years in total
 
-### START: Plot Co2 emission across years in China
-df_co2_emsn_tot.loc['Column_Total']= df_co2_emsn_tot.sum(numeric_only=True, axis=0)
-# create a new figure
-plt.figure(figsize=(8,5))
-# add a stylesheet
-plt.style.use('ggplot')
-# plot the grapgh
-df_co2_emsn_tot.at['Column_Total','Country Name']='Total'
-df_co2_emsn_tot_row = df_co2_emsn_tot[df_co2_emsn_tot['Country Name'] == 'Total']
-df_co2_emsn_tot_row_tmp = df_co2_emsn_tot_row.loc[:,['1990', '1995', '2000', '2005', '2010', '2015', '2020']]
-df_co2_emsn_tot_row_tp = df_co2_emsn_tot_row_tmp.transpose()
-df_co2_emsn_tot_row_tp['Year'] = df_co2_emsn_tot_row_tp.index
+#Derive mathematical statistics
+df_stats = df_fnl[df_fnl['Indicator Name'] == 'CO2 emissions (kt)']
+chn_mean = df_stats[df_stats['Country Name'] == 'China']
 
-# create a new figure
-plt.figure(figsize=(8,5))
-# add a stylesheet
-plt.style.use('ggplot')
-# plot the grapgh
-df_co2_emsn_tot_row_tp.plot.bar('Year', 'Column_Total', color=(0.2, 0.4, 0.6, 0.6))
-# labeling the graph
-plt.ylabel('Co2 emmision')
-plt.xlabel('Year')
-plt.legend(loc = 'best')
-# save plot as .png
-plt.savefig('Total Co2 emission of period 1990-2020.png', dpi=300, bbox_inches='tight')
+otp = calculateStats(chn_mean)
+    
+print('\nAverage Co2 emission of China per year is ', otp[2])
+print('\nMaximum Co2 emission recorded in China is ', otp[0])
+print('\nMinimum Co2 emission recorded in China is ', otp[1])
+
+''' Plot: 5
+    Plot type: correlation heatmap of USA
+    Plot name: Average Co2 emission per year'''
+df_cor = df_fnl[df_fnl['Country Name'] == 'USA']
+df_cor = df_cor.loc[:,['Indicator Name', '1990', '1995', '2000', '2005', '2010', '2015', '2020']]
+df_cor = df_cor.set_index('Indicator Name').transpose()
+df_cor.rename(columns = {'CO2 emissions (kt)':'CO2 emission', 
+                        'Urban population':'Urban population', 
+                        'Population, total':'Total population', 
+                        'Forest area (% of land area)':'Forest area',
+                        'Arable land (% of land area)':'Arable area', 
+                        'Total greenhouse gas emissions (kt of CO2 equivalent)':'Greenhouse gas emission', 
+                        'Methane emissions (kt of CO2 equivalent)':'Methane emission', 
+                        'Mortality rate, under-5 (per 1,000 live births)':'Mortality rate (Under 5)'
+                        }, inplace = True)
+print(df_cor.corr())
+# plotting correlation heatmap
+dataplot = sb.heatmap(df_cor.corr(), cmap="YlGnBu", annot=True)
+  
+# displaying heatmap
 plt.show()
-### END: Plot Co2 emission across years in China
